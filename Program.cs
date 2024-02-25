667acdb1430d51c48e93dd111adf8e0137af73a1
vlad <vlad@DESKTOP-KR5OOL4>
2024-02-25 20:50:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x, y;
+        Console.Write("Введите значение x: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        y = 5 * x + 20;
+        Console.WriteLine("Значение выражения y *= 5*x + 20 равно: \n" + y);
+    }
+}
